bbfa65356581feaf5db96495b4f45fc9eeccc317
Phil Lui <phillui37@gmail.com>
2024-06-25 10:26:28 +0800

    - optimize DefaultWebDavRepository code

--- app/src/main/java/xyz/kgy_production/webdavebookmanager/data/DefaultWebDavRepository.kt
+++ app/src/main/java/xyz/kgy_production/webdavebookmanager/data/DefaultWebDavRepository.kt
@@ -3,6 +3,7 @@ package xyz.kgy_production.webdavebookmanager.data
 import android.util.Log
 import arrow.core.Option
 import arrow.core.getOrElse
+import arrow.core.raise.option
 import arrow.core.toOption
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
@@ -45,7 +46,17 @@ class DefaultWebDavRepository @Inject constructor(
         loginId: String
     ): Option<WebDavModel> {
         return withContext(dispatcher) {
-            dbDAO.getByUrlAndLoginId(url, loginId)?.toModel().toOption()
+            option {
+                val _loginId = loginId
+                    .encrypt()
+                    .onNone { Log.e("WebDavRepo::getEntryByUrlAndLoginId", "Encrypt fail") }
+                    .bind()
+                dbDAO.getByUrlAndLoginId(url, _loginId)
+                    ?.toModel()
+                    .toOption()
+                    .onNone { Log.d("WebDavRepo::getEntryByUrlAndLoginId", "Entry no exists") }
+                    .bind()
+            }
         }
     }
 
@@ -55,23 +66,14 @@ class DefaultWebDavRepository @Inject constructor(
         loginId: Option<String>,
         password: Option<String>,
     ) {
-        Log.d(">>>", "$name;$url;$loginId;$password")
         withContext(dispatcher) {
             var uuid = UUID.randomUUID().toString()
             while (getEntryByUuid(uuid).isSome())
                 uuid = UUID.randomUUID().toString()
-            val _loginId = loginId.flatMap { it.encrypt() }
-                .onNone {
-                    Log.e("WebDavRepo::createEntry", "Encrypt fail")
-                }
-                .getOrElse { "" }
-            val _pwd = password.flatMap { it.encrypt() }
-                .onNone {
-                    Log.e("WebDavRepo::createEntry", "Encrypt fail")
-                }
-                .getOrElse { "" }
-            dbDAO.insert(
-                WebDavEntity(
+            option {
+                val _loginId = loginId.flatMap(String::encrypt).bind()
+                val _pwd = password.flatMap(String::encrypt).bind()
+                val webDavEntity = WebDavEntity(
                     name = name.getOrElse { "" },
                     url = url,
                     loginId = _loginId,
@@ -79,7 +81,10 @@ class DefaultWebDavRepository @Inject constructor(
                     isActive = true,
                     uuid = uuid
                 )
-            )
+                dbDAO.insert(webDavEntity)
+            }.onNone {
+                Log.e("WebDavRepo::createEntry", "Encrypt fail")
+            }
         }
     }
 
@@ -99,18 +104,10 @@ class DefaultWebDavRepository @Inject constructor(
         uuid: String,
     ) {
         withContext(dispatcher) {
-            val _loginId = loginId.flatMap { it.decrypt() }
-                .onNone {
-                    Log.e("WebDavRepo::createEntry", "Decrypt fail")
-                }
-                .getOrElse { "" }
-            val _pwd = password.flatMap { it.decrypt() }
-                .onNone {
-                    Log.e("WebDavRepo::createEntry", "Decrypt fail")
-                }
-                .getOrElse { "" }
-            dbDAO.upsert(
-                WebDavEntity(
+            option {
+                val _loginId = loginId.flatMap(String::decrypt).bind()
+                val _pwd = password.flatMap(String::decrypt).bind()
+                val entity = WebDavEntity(
                     id = id,
                     name = name.getOrElse { "" },
                     url = url,
@@ -119,36 +116,47 @@ class DefaultWebDavRepository @Inject constructor(
                     isActive = isActive,
                     uuid = uuid
                 )
-            )
+                dbDAO.upsert(entity)
+            }.onNone {
+                Log.e("WebDavRepo::createEntry", "Decrypt fail")
+            }
         }
     }
 
     override suspend fun updateEntry(model: WebDavModel) {
         withContext(dispatcher) {
-            getEntryById(model.id) ?: return@withContext
-            dbDAO.upsert(model.toEntity())
-        }
+            option {
+                getEntryById(model.id).bind()
+                dbDAO.upsert(model.toEntity())
+            }
+        }.onNone { Log.w("WebDavRepo::updateEntry", "id not exists, op has stopped") }
     }
 
     override suspend fun deactivateEntry(id: Int) {
         withContext(dispatcher) {
-            getEntryById(id) ?: return@withContext
-            dbDAO.setIsActive(id, false)
-        }
+            option {
+                getEntryById(id).bind()
+                dbDAO.setIsActive(id, false)
+            }
+        }.onNone { Log.w("WebDavRepo::deactivateEntry", "id not exists, op has stopped") }
     }
 
     override suspend fun activateEntry(id: Int) {
         withContext(dispatcher) {
-            getEntryById(id) ?: return@withContext
-            dbDAO.setIsActive(id, false)
-        }
+            option {
+                getEntryById(id).bind()
+                dbDAO.setIsActive(id, true)
+            }
+        }.onNone { Log.w("WebDavRepo::activateEntry", "id not exists, op has stopped") }
     }
 
     override suspend fun deleteEntry(id: Int) {
         withContext(dispatcher) {
-            getEntryById(id) ?: return@withContext
-            dbDAO.deleteById(id)
-        }
+            option {
+                getEntryById(id).bind()
+                dbDAO.deleteById(id)
+            }
+        }.onNone { Log.w("WebDavRepo::deleteEntry", "id not exists, op has stopped") }
     }
 
 }
